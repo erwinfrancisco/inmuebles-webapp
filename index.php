7ba937234cda00b987ed566c42ea073087af48cb
erwinfrancisco <erwin.fco@gmail.com>
2022-06-22 18:48:10 -0500

    Se modifica el logo por 200px

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style><?php include 'css/estilos.css'?></style>
-    <title>Index</title>
-</head>
-<body>
-
-    <?php include './main.php';?>
-    
-</body>
-</html>
+<?php include './main.php';?>
--- main.php
+++ main.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=StyleSheet src="./css/estilos.css" type="text/css"> 
+    <style><?php include 'css/estilos.css'?></style>
     <title>Inmuebles de la Fiscalía General de Justicia del Estado de Tamaulipas</title>
 </head>
 <body>
     <header>
-        <img src="./img/header_vector.png" width="100%" height="140" alt="juana" id="vector_header">
+        <img src="./img/header_vector.png" width="100%" height="140">
         <div class="logo">
-            <img src="./img/logo_fgj_blanco.png" width="200" alt="juana">
+            <img src="./img/logo_fgj_blanco.png" width="100%" alt="Fiscalía General de Justicia del Estado de Tamaulipas">
         </div>
 
         <nav></nav>
