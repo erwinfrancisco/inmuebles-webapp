6a7a408ab46e026148063e1bc8e85fad895f7788
erwinfrancisco <erwin.fco@gmail.com>
2022-06-26 23:27:25 -0500

    se agrega interaccion get y regiones.php

---
+++ haber.php
@@ -0,0 +1,6 @@
+<?php
+include './main.php';
+if(isset($_REQUEST['region'])){
+    echo "Region nnUEVO Laredo";
+}
+echo isset($_REQUEST['region']);
---
+++ regiones.php
@@ -0,0 +1,33 @@
+<?php
+
+include("./main.php");
+
+if(isset($_REQUEST['fiscalia-nuevo-laredo'])){
+    echo "<h1>estas en nuevo laredo</h1>";
+}elseif(isset($_REQUEST['fiscalia-matamoros'])){
+    echo "<h1>estas en matamoros</h1>";
+}elseif(isset($_REQUEST['fiscalia-reynosa'])){
+    echo "<h1>estas en reynosa</h1>";
+}elseif(isset($_REQUEST['fiscalia-victoria'])){
+    echo "<h1>estas en victoria</h1>";
+}elseif(isset($_REQUEST['fiscalia-el-mante'])){
+    echo "<h1>estas en mante</h1>";
+}elseif(isset($_REQUEST['fiscalia-zona-sur'])){
+    echo "<h1>estas en Zona Sur</h1>";
+}else{
+    echo "verifica tu solicitud";
+}
+echo "<pre>";
+echo print_r($_REQUEST);
+echo "</pre>";
+;
+
+echo "<hr>";
+
+$array = ['Imelda', 'Francisco', 'Bryan', 'Fátima', 'Erwin', 'Keily', 'Pedrito'];
+
+foreach($array as $nombre){
+    printf("<h3>Yo {$nombre} y soy parte de la familia Flores.</h3>");
+    echo $array[1];
+    echo $array[6];
+};
