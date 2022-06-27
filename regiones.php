262c3298d989d993c5fd1f44fb30bed22ef19723
erwinfrancisco <erwin.fco@gmail.com>
2022-06-27 06:57:30 -0500

    se modifico la estructura de urls de las regiones

--- regiones.php
+++ regiones.php
@@ -2,21 +2,28 @@
 
 include("./main.php");
 
-if(isset($_REQUEST['fiscalia-nuevo-laredo'])){
-    echo "<h1>estas en nuevo laredo</h1>";
-}elseif(isset($_REQUEST['fiscalia-matamoros'])){
-    echo "<h1>estas en matamoros</h1>";
-}elseif(isset($_REQUEST['fiscalia-reynosa'])){
-    echo "<h1>estas en reynosa</h1>";
-}elseif(isset($_REQUEST['fiscalia-victoria'])){
-    echo "<h1>estas en victoria</h1>";
-}elseif(isset($_REQUEST['fiscalia-el-mante'])){
-    echo "<h1>estas en mante</h1>";
-}elseif(isset($_REQUEST['fiscalia-zona-sur'])){
-    echo "<h1>estas en Zona Sur</h1>";
-}else{
-    echo "verifica tu solicitud";
+if(isset($_REQUEST['fiscalia'])){
+    if($_REQUEST['fiscalia'] == "nuevo-laredo"){
+        echo "<h1>estas en nuevo laredo</h1>";
+    }elseif($_REQUEST['fiscalia'] == "matamoros"){
+        echo "<h1>estas en matamoros</h1>";
+    }elseif($_REQUEST['fiscalia'] == "reynosa"){
+        echo "<h1>estas en reynosa</h1>";
+    }elseif($_REQUEST['fiscalia'] == "victoria"){
+        echo "<h1>estas en victoria</h1>";
+    }elseif($_REQUEST['fiscalia'] == "el-mante"){
+        echo "<h1>estas en mante</h1>";
+    }elseif($_REQUEST['fiscalia'] == "zona-sur"){
+        echo "<h1>estas en Zona Sur</h1>";
+    }else{
+        echo "verifica tu solicitud";
+    }
+
+
+
 }
+
+
 echo "<pre>";
 echo print_r($_REQUEST);
 echo "</pre>";
