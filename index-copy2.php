75c58bc223be4f466e53c38d85399cdc42e741c2
erwinfrancisco <erwin.fco@gmail.com>
2022-07-15 20:32:40 -0500

    Index copy ok responsivo
    Falta realizar el responsive del index copy 2 y con el css 2

--- index-copy2.php
+++ index-copy2.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="html-principal"lang="en">
+<html class="html"lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,45 +8,47 @@
     <style><?= include_once('./css/materialize.css')?></style>
     <style><?= include_once('./css/estilos2.css')?></style>
     
-    <title>Inmuebles de la Fiscalía General de Justicia del Estado de Tamaulipas</title>
+    <title>Fiscalía General de Justicia del Estado de Tamaulipas</title>
 </head>
-<body id="body_index">
+<body id="body-background-blue">
 
     <!-- Encabezado -->
     <header class="contenedor-header-blanco">
         <!-- <div class="contenedor-header-blanco"> -->
             <div class="contenedor-logo-azul"></div>
             <div class="contenedor-buscador">
-                <form action="#" method="post">
+                <form action="index.php" method="get">
                     <input type="text" id="buscador" name="buscador" placeholder="Buscar área o inmueble" name="buscador">
-                    <input type="submit" class="boton-buscar waves-effect waves-light btn blue accent-2" value="Buscar">
+                    <button class="btn waves-effect waves-light blue accent-2 btnavbar" type="submit" name="action">Buscar<span class="material-icons tiny right valign-wrapper">search</span></button>
                 </form>
             </div>
             <div class="naveg-header">
+                <span class="material-icons tiny right valign-wrapper blue-text accent-2">business</span>
                 <a href="./regiones-main.php" class="waves-effect waves-light btn blue accent-2 btnavbar">Inmuebles</a>
+                <span class="material-icons tiny right valign-wrapper blue-text accent-2">my_location</span>
                 <a href="./regiones-main.php" class="waves-effect waves-light btn blue accent-2 btnavbar">Áreas</a>
+                <span class="material-icons tiny right valign-wrapper blue-text accent-2">insert_chart</span>
                 <a href="./regiones-main.php" class="waves-effect waves-light btn blue accent-2 btnavbar">Reportes</a>  
             </div>
-        
     </header>
     
     
-    <div class="contenedor-imagen">
-        <div>
-            <h4 style="color:white;">Consulta de inmuebles FGJ:</h4>
-            <img src="img/ubicacion-mapa.png" width="300px">
+    <div class="contenedor-imagen-index">
+        <div class="mapa-ubicacion">
+            <h4 style="color:white;text-align:center;">Consulta de inmuebles FGJ:</h4>
+            <img src="img/ubicacion-mapa.png" width="300px" style="text-align:center;">
         </div>
-        <div class="consulta-region" style="background-color:white; width:200px; border-radius:20px;margin-left:120px">
+        <div class="consulta-region" style="background-color:white; width:200px; border-radius:20px;">
             <img src="img/regiones.png" width="100px">
             
             <p>Fiscalías de Distrito</p>
-            <a href="./regiones-main.php" class="waves-effect waves-light btn blue accent-2">buscar</a>
+            <a href="./regiones-main.php" class="waves-effect waves-light btn blue accent-2">consultar</a>
         </div>
-        <div class="consulta-municipio" style="background-color:white;width:200px;border-radius:20px;margin-left:40px">
+        <div class="consulta-municipio" style="background-color:white;width:200px;border-radius:20px;">
             <img src="img/municipios.png" width="100px">
             
             <p>Municipios</p>
-            <a href="./municipios-main.php" class="waves-effect waves-light btn blue accent-2">buscar</a>
+            <a href="./municipios-main.php" class="waves-effect waves-light btn blue accent-2">consultar</a>
         </div>
     </div>
 
